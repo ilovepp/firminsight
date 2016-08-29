9af7fe3770d942b2a59804680b86283e516c7732
ilovepp <chenyu9043@iie.ac.cn>
2016-08-29 18:01:20 +0800

    V1.1

--- BinRelation/BinRelation.sh
+++ BinRelation/BinRelation.sh
@@ -3,7 +3,12 @@ eval `tail -n +2 ../GLOBAL_CONFIG`
 eval `tail -n +2 CONFIG`
 target_firm_id=$1
 target_bin="$2"
-#echo "serach similar binarys of FirmID:${target_firm_id}'s BinName:${target_bin}"
+
+if [ $# != 2 ];then
+    echo "USAGE:$0 target_firm_id target_bin"
+    exit 1
+fi
+
 mongo_cmd="db.${MONGO_FIRM_COLLECTION_NAME}.find({FirmID:${target_firm_id},\"BinFiles.BinName\":\"${target_bin}\"},{BinFiles:1,_id:0}).toArray();"
 mongo_return=$(echo "$mongo_cmd"|mongo ${MONGO_IP}:${MONGO_PORT}/${MONGO_DATABASE} --quiet --shell 2>/dev/null|grep -E "\"BinName\" :|\"ssdeep\" :")
 [ -z "$mongo_return" ] && echo "FirmID:${target_firm_id}'s BinName:${target_bin} not in the database!" && exit
--- FunRelation/FunRelation.sh
+++ FunRelation/FunRelation.sh
@@ -16,18 +16,17 @@ search_firm_id="$1"
 search_bin_name="$2"
 vul_id="$3"
 
+search_firm_name=$(mongocmd "db.${MONGO_FIRM_COLLECTION_NAME}.find({FirmID:${search_firm_id}}).toArray()"|grep FirmName|awk -F\" '{print $4}')
 search_save_bin=$(mongocmd "db.${MONGO_FIRM_COLLECTION_NAME}.find({FirmID:${search_firm_id}},{_id:0,BinFiles:{\$elemMatch:{BinName:\"${search_bin_name}\"}}})"|sed -e 's/, /\n/g'|grep BinSave|awk -F\" '{print $4}')
 target_save_bin=$(mongocmd "db.${MONGO_VUL_COLLECTION_NAME}.find({VulID:${vul_id}}).toArray()"|grep BinSave|awk -F\" '{print $4}')
 search_instset=$(mongocmd "db.${MONGO_FIRM_COLLECTION_NAME}.find({FirmID:${search_firm_id}},{_id:0,BinFiles:{\$elemMatch:{BinName:\"${search_bin_name}\"}}})"|sed -e 's/, /\n/g'|grep BinInstSet|awk -F\" '{print $4}')
 target_instset=$(mongocmd "db.${MONGO_VUL_COLLECTION_NAME}.find({VulID:${vul_id}}).toArray()"|grep BinInstSet|awk -F\" '{print $4}')
 target_fun_name=$(mongocmd "db.${MONGO_VUL_COLLECTION_NAME}.find({VulID:${vul_id}}).toArray()"|grep FuncName|awk -F\" '{print $4}')
 
-echo $search_save_bin
-echo $target_save_bin
-echo $search_instset
-echo $target_instset
 [ -z "$search_save_bin" -o -z "$target_save_bin" ] && echo "binary not in the database!" && exit
 [ -z "$target_fun_name" ] && echo "function not in the binary" && exit
+BinMD5=`md5sum "$BINARY_STORE_PATH/$search_save_bin"|awk '{print $1}'`
+[ $(mongocmd "db.${MONGO_RELATION_COLLECTION_NAME}.findOne({VulID:${vul_id},BinMD5:\"${BinMD5}\"});"|tail -1) != "null" ] && echo "relation result already in database" && mongocmd "db.${MONGO_RELATION_COLLECTION_NAME}.findOne({VulID:${vul_id},BinMD5:\"${BinMD5}\"},{_id:0,Result:1});" && exit
 if [ -n "`file "$BINARY_STORE_PATH/$search_save_bin"|grep ELF`" ];then
     $IDA_PATH -B -o$DECOMPRESS_TEMP_PATH/search.i64 "$BINARY_STORE_PATH/$search_save_bin"
 else
@@ -56,8 +55,15 @@ fi
 [ ! -f $VUL_BINARY_STORE_PATH/$target_save_bin.i64 -o ! -f $DECOMPRESS_TEMP_PATH/search.i64 ] && echo "ida can't produce i64 file" && exit
 
 python Core/bugSearch.py $DECOMPRESS_TEMP_PATH/search.i64 $VUL_BINARY_STORE_PATH/$target_save_bin.i64 $target_fun_name
-cat result.json
-rm $DECOMPRESS_TEMP_PATH/search.* 
-#./FunRelation.sh 1917 31F 5
 
+rm $DECOMPRESS_TEMP_PATH/search.* 
+
+[ ! -s  result.json -o `cat result.json` = "{}" ] && echo "no matched function" && exit
+
+cat result.json
+Time=`date`
+Result=`cat result.json|sed "s/\"//g"`
+mongocmd "db.${MONGO_RELATION_COLLECTION_NAME}.insert({VulID:${vul_id},FirmID:${search_firm_id},FirmName:\"${search_firm_name}\",BinName:\"${search_save_bin}\",BinMD5:\"${BinMD5}\",Time:\"${Time}\",Result:\"${Result}\"});"
+
+#./FunRelation.sh 1917 31F 5
 #./FunRelation.sh 4615 cgibin 3
